f28b3f47951b4bb17947d82b09d389999d62196e
Idstein, Paul <pidstein@agtinternational.com>
2019-02-20 21:26:42 +0100

    Fix default activity; start correct background foreground service

--- app/src/main/java/info/idstein/camera/rtsp_background_camera/LiveActivity.java
+++ app/src/main/java/info/idstein/camera/rtsp_background_camera/LiveActivity.java
@@ -22,23 +22,13 @@ public class LiveActivity extends Activity {
         setContentView(R.layout.live_activity);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
-
         // Sets the port of the RTSP server to 1234
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
         editor.putString(RtspServer.KEY_PORT, String.valueOf(8080));
         editor.commit();
 
-        // Configures the SessionBuilder
-        SessionBuilder.getInstance()
-                .setSurfaceView(mSurfaceView)
-                .setPreviewOrientation(90)
-                .setContext(getApplicationContext())
-                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
-                .setVideoEncoder(SessionBuilder.VIDEO_H264);
-
         // Starts the RTSP server
-        this.startService(new Intent(this, RtspServer.class));
+        this.startService(new Intent(this, BackgroundCameraService.class));
 
     }
 }
